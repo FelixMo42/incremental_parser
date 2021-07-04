39bf292e400ee7f95eb4ea2e08f6f41f165b3110
FelixMo42 <felix@tacocat.com>
2021-07-03 17:21:14 -0700

    started rework, made tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,247 +1,132 @@
-mod source;
+use std::{iter::Peekable, ops::RangeInclusive, str::CharIndices};
 
-use crate::source::Source;
+pub mod source;
+pub mod language;
 
-// Node struct
-#[derive(Debug)]
-pub struct Node {
-    range: (usize, usize),
-    info: NodeType,
-}
+struct Node (Vec<(RangeInclusive<char>, usize)>, bool);
 
-#[derive(Debug, PartialEq)]
-enum NodeType {
-    Error,
-    Symbol,
-}
+type Cursor<'a> = Peekable<CharIndices<'a>>;
 
-impl Node {
-    fn error() -> Node {
-        Node {
-            range: (0, 0),
-            info: NodeType::Error
-        }
-    }
+fn parse(nodes: &Vec<Node>, cursor: &mut Cursor) -> bool {
+    let mut index = 0;
 
-    fn symbol() -> Node {
-        Node {
-            range: (0, 0),
-            info: NodeType::Symbol
-        }
-    }
-}
-
-// Rule trait
-pub trait Rule {
-    fn parse(&self, source: &mut Source) -> Node;
-}
-
-type RuleId = usize;
-
-//
-struct PlaceholderRule {}
-
-impl Rule for PlaceholderRule {
-    fn parse(&self, _source: &mut Source) -> Node {
-        unreachable!();
-    }
-}
-
-// Symbol Rule
-impl Rule for String {
-    fn parse(&self, source: &mut Source) -> Node {
-        source.skip_white_space();
-        for chr in self.chars() {
-            if !source.eat_char(chr) {
-                return Node::error()
-            }
-        }
-        return Node::symbol();
-    }
-}
-
-// Loop Rule
-struct Lots {
-    rule: RuleId
-}
-
-impl Lots {
-    fn new(rule: RuleId) -> Box<Lots> {
-        return Box::new( Lots {
-            rule
-        } );
-    }
-}
-
-impl Rule for Lots {
-    fn parse(&self, source: &mut Source) -> Node {
-        while let Node { range: _, info: NodeType::Symbol } = source.eat_rule(self.rule) {} 
-        return Node::symbol();
-    }
-}
-
-//
-struct EndOfFile {}
-
-impl Rule for EndOfFile {
-    fn parse(&self, source: &mut Source) -> Node {
-        source.skip_white_space();
-        if source.is_end_of_file() {
-            return Node::symbol();
-        } else {
-            return Node::error();
-        }
-    }
-}
-
-// 
-struct Union {
-    rules: Vec<usize>
-}
-
-impl Union {
-    fn new(rules: Vec<usize>) -> Box<Union> {
-        return Box::new( Union {
-            rules
-        } );
-    }
-}
-
-impl Rule for Union {
-    fn parse(&self, source: &mut Source) -> Node {
-        for rule in self.rules.iter() {
-            source.skip_white_space();
-            
-            if source.eat_rule(*rule).info == NodeType::Error {
-                return Node::error(); 
-            }
-        }
-
-        return Node::symbol();
-    }
-}
-
-struct Ident {}
-
-impl Rule for Ident {
-    fn parse(&self, source: &mut Source) -> Node {
-        let range = 'a'..='z';
-
-        source.skip_white_space();
-
-        if !source.eat_char_range(&range) {
-            return Node::error();
-        }
-
-        while source.eat_char_range(&range) {}
+    'main: loop {
+        let node = &nodes[index];
         
-        return Node::symbol();
-    }
-}
+        if let Some((_, chr)) = cursor.peek() {
+            for (case, i) in &node.0 {
+                if case.contains(chr) {
+                   index = i.clone();
 
-struct Opts {
-    opts: Vec<RuleId>
-}
+                   cursor.next();
 
-impl Opts {
-    fn new(opts: Vec<RuleId>) -> Box<Opts> {
-        return Box::new(Opts {
-            opts
-        })
-    }
-}
-
-impl Rule for Opts {
-    fn parse(&self, source: &mut Source) -> Node {
-        for opt in self.opts.iter() {
-            if source.eat_rule(*opt).info == NodeType::Symbol {
-                return Node::symbol()
+                   continue 'main;
+                }
             }
         }
-        return Node::error();
-    }
-}
 
-pub struct Language {
-    pub rules: Vec<Box<dyn Rule>>
-}
-
-impl Language {
-    pub fn new() -> Language {
-        return Language {
-            rules: vec![]
+        if node.1 {
+            return true;
         }
-    }
-
-    pub fn add_rule(&mut self, rule: Box<dyn Rule>) -> usize {
-        self.rules.push(rule);
-        return self.rules.len() - 1;
-    }
-    
-    pub fn set_rule(&mut self, index: usize, rule: Box<dyn Rule>) {
-        self.rules[index] = rule;
+        
+        return false;
     }
 }
 
-// Main
+struct Symbol<'a> {
+    span: (usize, usize),
+    kind: &'a Token,
+}
+
+struct Token(String, Vec<Node>);
+
+impl Token {
+    pub fn name(&self) -> &String {
+        return &self.0;
+    } 
+
+    pub fn nodes(&self) -> &Vec<Node> {
+        return &self.1;
+    } 
+}
+
 fn main() {
+    let src = "let name = ";
 
-    let mut lang = Language::new();
+    let const_let = Token("let".to_string(), vec![
+        Node(vec![('l'..='l', 1)], false),
+        Node(vec![('e'..='e', 2)], false),
+        Node(vec![('t'..='t', 3)], false),
+        Node(vec![], true),
+    ]);
 
-    let placeholder = Box::new(PlaceholderRule {});
+    let ident = Token("ident".to_string(), vec![
+        Node(vec![
+            ('a'..='z', 0),
+            ('A'..='Z', 0),
+            ('_'..='_', 0),
+            ('\''..='\'', 0),
+        ], true),
+    ]);
 
-    // ident
-    let ident = lang.add_rule(Box::new(Ident {}));
+    let whitespace = Token("whitespace".to_string(), vec![
+        Node(vec![
+            ('\t'..=' ', 0)
+        ], true)
+    ]);
 
-    // params
-    let params = lang.add_rule(Lots::new(ident));
+    let punctuation = Token("punctuation".to_string(), vec![
+        Node(vec![
+            ('!'..='/', 0),
+            (':'..='@', 0),
+        ], true)
+    ]);
 
-    // value
-    let value = lang.add_rule(placeholder);
-
-    // values
-    let values = lang.add_rule(Lots::new(value));
-
-    // call
-    let open_paren = lang.add_rule(Box::new("(".to_string()));
-    let close_paren = lang.add_rule(Box::new(")".to_string()));
-    let call = lang.add_rule(Union::new( vec![
-        open_paren,
+    let tokens = vec![
+        whitespace,
+        const_let,
+        punctuation,
         ident,
-        values,
-        close_paren
-    ] ));
-    
-    // func
-    let open_braket = lang.add_rule(Box::new("[".to_string()));
-    let close_braket = lang.add_rule(Box::new("]".to_string()));
-    let func = lang.add_rule(Union::new( vec![
-        ident,
-        open_braket,
-        params,
-        close_braket,
-        value,
-    ] ));
-        
-    // add value
-    lang.set_rule(value, Opts::new(vec![
-        ident,
-        call
-    ]));
-    
-    let funcs = lang.add_rule(Lots::new(func));
-    let eof = lang.add_rule(Box::new(EndOfFile {}));
-    let file = lang.add_rule(Union::new( vec![
-        funcs,
-        eof
-    ] ));
-    
-    let src = &mut Source::new(&lang, r#"
-        bla [ x y ] (add x y)
-        main [args] (bla (unwrap args))
-    "#);
+    ];
 
-    let prs = src.eat_rule(file);
+    let mut cursor = src.char_indices().peekable();
 
-    println!("{:?}", prs);
+    let mut symbols: Vec<Symbol> = vec![];
+
+    while cursor.peek().is_some() {
+        for token in &tokens {
+            // Keep a copy of the Cursor in case the parse fails.
+            let mut save = cursor.clone();
+
+            // Try to parse the token.
+            let mut success = parse(token.nodes(), &mut cursor);
+
+            // Make that at least one token has been parsed.
+            if save.peek() == cursor.peek() {
+                success = false;
+            }
+
+            // If the parse failed, restore the old cursor.
+            if !success {
+                cursor = save.clone();
+            }
+
+
+            if success {
+                let start = save.peek().unwrap().0;
+                let end = cursor.peek().map(|(i, _chr)| i.clone()).unwrap_or(src.len());
+
+                symbols.push(Symbol {
+                    span: (start, end),
+                    kind: token,
+                });
+
+                break;
+            }
+        }
+    }
+
+    for symbol in symbols {
+        println!("({}, {}) {}", symbol.span.0, symbol.span.1, symbol.kind.name());
+    }
 }
